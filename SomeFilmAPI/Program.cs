82a245d22ba5587eaf3b961262d43551964729a5
glenk <glenk@DESKTOP-OI4QHMF>
2026-03-01 22:09:55 +0300

    Добавлен MoviesController.cs, а также подключение к БД через npgsql в builder

--- SomeFilmAPI/Program.cs
+++ SomeFilmAPI/Program.cs
@@ -1,11 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using SomeFilmAPI;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
+builder.Services.AddDbContext<SomeFilmContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddControllers();
 builder.Services.AddOpenApi();
-
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
